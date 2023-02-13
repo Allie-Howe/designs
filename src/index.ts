806322a3cc7ad97e864b5d6055c38af307d0f0f2
Allie Howe <howeisallie@gmail.com>
2023-02-13 23:23:25 +0000

    draw now its own function which calls nested draw functions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import P5 from 'p5'
+import { love } from './love';
 import { rotatingPlus } from './rotatingPlus';
 
 let animIndex = 0;
 const drawFnArray: ((p5: P5) => (() => void))[] = [
+  love,
   rotatingPlus,
 ];
 
@@ -16,9 +18,11 @@ const sketch = (p5: P5) => {
     p5.frameRate(60)
 
     p5.rectMode(p5.CENTER)
+    p5.ellipseMode(p5.CENTER);
+    p5.noStroke();
   }
 
-  p5.draw = drawFnArray[animIndex % drawFnArray.length](p5);
+  p5.draw = () => drawFnArray[animIndex % drawFnArray.length](p5)();
   p5.mousePressed = () => animIndex++;
 }
 
