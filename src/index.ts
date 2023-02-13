a85ebd9480f295accfff60770997602a30f21a1a
Allie Howe <howeisallie@gmail.com>
2023-02-13 22:31:22 +0000

    cycles which draw function should run on mouse press

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import P5 from 'p5'
+import { rotatingPlus } from './rotatingPlus';
+
+let animIndex = 0;
+const drawFnArray: ((p5: P5) => (() => void))[] = [
+  rotatingPlus,
+];
 
 const sketch = (p5: P5) => {
   p5.windowResized = () => {
@@ -12,12 +18,8 @@ const sketch = (p5: P5) => {
     p5.rectMode(p5.CENTER)
   }
 
-  p5.draw = () => {
-    p5.background(0);
-    p5.translate(p5.width / 2, p5.height / 2)
-
-    p5.rect(0, 0, 100, 100)
-  }
+  p5.draw = drawFnArray[animIndex % drawFnArray.length](p5);
+  p5.mousePressed = () => animIndex++;
 }
 
 new P5(sketch)
